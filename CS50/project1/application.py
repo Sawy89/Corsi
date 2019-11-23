3ad9f27611e0433d4e39407ce57715bfc9230929
Sawy89 <ddeennyy@hotmail.it>
2019-11-23 16:17:43 +0100

    search page for books

--- CS50/project1/application.py
+++ CS50/project1/application.py
@@ -8,7 +8,7 @@ Need a txt file with the key
 """
 
 # %% Import
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_session import Session
 # from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -37,4 +37,28 @@ db = DBconnection()
 @app.route("/")
 @loginRequired
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return redirect(url_for('search'))
+
+
+@app.route("/search", methods=['POST','GET'])
+@loginRequired
+def search():
+    '''
+    Page for searching the book in the database
+    Method:
+        - GET = FORM for searching
+        - POST = FORM for searching again + result
+    '''
+    if request.method == 'POST':
+        # Query with input: download books
+        books = db.execute("SELECT * FROM books WHERE LOWER(isbn) LIKE :isbn AND LOWER(title) LIKE :title AND LOWER(author) LIKE :author",
+                              {'isbn':'%'+(request.form['isbn']).lower()+'%', 
+                              'title':'%'+(request.form['title']).lower()+'%',
+                              'author':'%'+(request.form['author']).lower()+'%'}).fetchall()
+        input_form = request.form
+    else:
+        books = None
+        input_form = None
+
+    return render_template('search.html', books=books, input_form=input_form)
