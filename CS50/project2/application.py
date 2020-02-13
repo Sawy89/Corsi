15de88e8f92fc7747b56cbed622448359f2404b4
Denny <terreno@eviso.it>
2020-02-13 18:38:47 +0100

    channel messages displayed and saved

--- CS50/project2/application.py
+++ CS50/project2/application.py
@@ -2,11 +2,13 @@ import os
 
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
+import datetime
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
-
+max_messages_per_channel = 100
 
 # %% Variables
 usernames = []  # for storing registered user
@@ -24,7 +26,7 @@ def login():
     return render_template("login.html")
 
 
-# %% API
+# %% API user
 @app.route("/register/check/<username>", methods=['GET'])
 def register_check(username):
     '''
@@ -55,6 +57,7 @@ def register_new():
         return jsonify({"registered": True}), 200
 
 
+# %% API channel
 @app.route("/channel/new/<channel>", methods=['GET'])
 def channel_new(channel):
     '''
@@ -62,14 +65,18 @@ def channel_new(channel):
     - if yes, return an error, 
     - otherwise create it and EMIT all channel
     '''
-    print(channels)
-    is_new = False if channel in channels else True
+    channels_name = [i['name'] for i in channels]
+    print(channels_name)
+    is_new = False if channel in channels_name else True
     if is_new == False:
         return jsonify({"already_present": True}), 200
     else:
-        channels.append(channel)
+        channel_dict = {'name': channel, 'messages':[{'message': 'Channel created', 
+                    'username': 'Admin',
+                    'insertdate': datetime.datetime.now()}]}
+        channels.append(channel_dict)
         socketio.emit("new channel", channel, broadcast=True)
-        return jsonify({"c": False}), 200
+        return jsonify({"already_present": False}), 200
 
 
 @app.route("/channel/getall", methods=['GET'])
@@ -77,5 +84,46 @@ def channel_all():
     '''
     Return the channels list
     '''
-    return jsonify({"channels": channels}), 200
+    channels_name = [i['name'] for i in channels]
+    return jsonify({"channels": channels_name}), 200
 
+
+@app.route("/channel/<channel>/getmessages", methods=['GET'])
+def channel_getmessages(channel):
+    '''
+    Return the list of messages of a specific channel
+    '''
+    for channel_dict in channels:
+        if channel_dict['name'] == channel:
+            return jsonify(channel_dict), 200
+    
+    return jsonify({"error": 'Channel not found!'}), 404
+
+
+@socketio.on("new message to server")
+def new_message(data):
+    '''
+    Get, save and emit a new message in a specific channel
+    '''
+    # Extract data
+    channel = data['channel']
+    new_message = {'message': data['message'], 
+                    'username': data['username'],
+                    'insertdate': datetime.datetime.now().strftime('%d-%m-%y %H:%M:%S')}
+
+    # save data
+    flag_found = False
+    for channel_dict in channels:
+        if channel_dict['name'] == channel:
+            flag_found = True
+            channel_dict['messages'].append(new_message)    # append new message
+            # delete (the first) if more than 100 messages
+            if len(channel_dict['messages']) > max_messages_per_channel:
+                channel_dict['messages'].pop(0)
+            break   # channel found!
+    if flag_found == False:
+        print("Channel "+channel+" doesn't exist!")
+        return jsonify({"error": 'Channel not found!'}), 404
+    
+    # emit the new message
+    socketio.emit("new message to client", new_message, broadcast=True)
--- CS50/project2/static/index.js
+++ CS50/project2/static/index.js
@@ -5,19 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Load all channel
     allChannel();
-    channelManag();
+
+    // Hide input messages
+    document.getElementById("input-div").style.visibility = "hidden";
 
     // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    // New Channel
-    channelButton();
+    // New Channel clicked
+    disableButton(document.querySelector('#new-channel'), document.querySelector('input[name="new-channel"]'));
     document.querySelector('#new-channel').onclick = function () {
         newChannelClicked();
     };
-    // Display new channel if the server emit!
+
+    // New channel added
     socket.on('new channel', data => {
         dispNewChannel(data);
+    });
+
+    // Disp new message
+    socket.on("new message to client", data => {
+        if (data['channel'] == localStorage.getItem('currentChannel'))
+            dispChannelMessage(data);
     });
 
 });
@@ -49,7 +58,7 @@ function loginCheck () {
 }
 
 
-// ALL CHANNEL
+// ALL CHANNEL: take all channels from server
 function allChannel () {
     // Create Get request
     var request = new XMLHttpRequest();
@@ -59,30 +68,31 @@ function allChannel () {
     // Result of request
     request.onload = () => {
         const data = JSON.parse(request.responseText);
-        if (request.status == 200)
+        if (request.status == 200) {
             data.channels.forEach(element => dispNewChannel(element));
+        }
         else
             alert('Bad request!');
     };
 };
 
 
-// NEW CHANNEL BUTTON
-function channelButton() {
+// FUNCTION DISABLE-ENABLE button on TEXT
+function disableButton(buttonE, textE) {
     // By default, submit button is disabled
-    document.querySelector('#new-channel').disabled = true;
+    buttonE.disabled = true;
 
     // Enable button only if there is text in the input field
-    document.querySelector('input[name="new-channel"]').onkeyup = () => {
-        if (document.querySelector('input[name="new-channel"]').value.length > 0)
-            document.querySelector('#new-channel').disabled = false;
+    textE.onkeyup = () => {
+        if (textE.value.length > 0)
+            buttonE.disabled = false;
         else
-            document.querySelector('#new-channel').disabled = true;
+            buttonE.disabled = true;
     };
 };
 
 
-// NEW CHANNEL
+// NEW CHANNEL: display the new channel
 function dispNewChannel(channelName) {
     // Create a new button with the cannel name and add it
     const channel = document.createElement('button');
@@ -91,15 +101,20 @@ function dispNewChannel(channelName) {
     channel.classList.add("channel-button");
     channel.innerHTML = channelName;
     document.querySelector('#channel-list').append(channel)
+    
+    // Add event listener for click on the channel
+    channel.addEventListener('click', () => {
+        channelSelected(channel.innerHTML);
+    });
 };
 
+// NEW CHANNEL CLICK: action when the new channel button is clicked
 function newChannelClicked () {
     // get the channel name (input) and send it back to server
     const channel = document.querySelector('input[name="new-channel"]').value;
     const request = new XMLHttpRequest();
     request.open('GET', '/channel/new/'+channel);
-    request.send(true);
-
+    
     // Result of request
     request.onload = () => {
         const data = JSON.parse(request.responseText);
@@ -109,22 +124,74 @@ function newChannelClicked () {
             alert("Channel "+channel+" created!");
     };
 
+    request.send(true);
+
     // empty the input text and disable button
     document.querySelector('input[name="new-channel"]').value = '';
     document.querySelector('#new-channel').disabled = true;    
 };
 
 
-// CHANNEL SELECTED
-function channelManag () {
-    // extract all channels and add event listener for channel clicked
-    const channels = document.querySelectorAll('.channel-button');
-    channels.forEach((channel) => {
-        channel.addEventListener('click', () => {
-          console.log("forEach worked");
-        });
-    });
+// CHANNEL SELECTED: what happens when a channel is selected
+function channelSelected (channel) {
+    console.log('yeah: '+channel);
+    
+    // Display channel
+    document.querySelector('#channel-title').innerHTML = channel;
+
+    // Get channel data
+    const request = new XMLHttpRequest();
+    request.open('GET', '/channel/'+channel+'/getmessages');
+
+    // Result of request
+    request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        if (request.status == 200 && channel == data['name']) {
+            document.querySelector('#channel-disp').innerHTML = '';
+            data['messages'].forEach(element => dispChannelMessage(element));
+            dispChannelMessagesInput(channel);
+            localStorage.setItem('currentChannel', channel);
+        }
+        else
+            alert("Channel "+channel+" data not found!");
+    };
+
+    request.send(true);
+
+};
+
+// CHANNEL MESSAGES display
+function dispChannelMessage (element) {
+    // display the message
+    const mes = document.createElement('div');
+    mes.innerHTML = element['message']
+    document.querySelector('#channel-disp').append(mes);
 };
 
 
+// CHANNEL MESSAGES display input
+function dispChannelMessagesInput (channel) {
+    // Display message input block
+    inputDiv = document.getElementById("input-div");
+    inputDiv.style.visibility = "visible";
 
+    // Enable & disable
+    const inputMessage = inputDiv.querySelector('input');
+    const inputButton = inputDiv.querySelector('button');
+    disableButton(inputButton, inputMessage);
+
+    // Onclick "Send" button
+    inputButton.onclick = function () {
+        const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+        
+        const data = {'channel': channel, 'message': inputMessage.value, 'username': localStorage.getItem('username')};
+        socket.emit("new message to server", data);
+
+        inputMessage.value = '';
+        inputButton.disabled = true;    
+    };
+};
+
+
+// ToDO: try to EMIT from server only to channels ?!?!
+// before this, check it's working well! I am not sure!!
