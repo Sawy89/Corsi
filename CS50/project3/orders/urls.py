840d964deee4b1e0d4ba6ec2b8f6731e2d948bae
Sawy89 <ddeennyy@hotmail.it>
2020-03-05 18:14:22 +0100

    user registragion (signup) page

--- CS50/project3/orders/urls.py
+++ CS50/project3/orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/signup", views.signup, name="signup"),
     path("myview", views.my_view, name="my_view")
 ]
