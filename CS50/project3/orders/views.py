f9edbd81d4a365ed69c2534ddb96eb287bd908c3
Denny <terreno@eviso.it>
2020-03-10 22:14:09 +0100

    display prices!

--- CS50/project3/orders/views.py
+++ CS50/project3/orders/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
@@ -33,11 +33,30 @@ def signup(request):
 
 @login_required
 def menu(request):
-    # Add info addition
+    # Add info addition & price
     DishProc = Dish.objects.all()
     for dish in DishProc:
         dish.addition_present = dish.addition.exists()
+        # add prices (if any, leave empty)
+        dish.prices = dict(list(DishPrice.objects.filter(dish=dish.id).all().values_list('dimension','price')))
+        for i in DishPrice.DimensionType.choices:
+            if i[0] not in dish.prices.keys():
+                dish.prices[i[0]] = ' - '
+    
+    # Add info small/large or normal
+    DishCatProc = DishCategory.objects.all()
+    for dishcat in DishCatProc:
+        list_dim = list(DishPrice.objects.filter(dish__category=dishcat.id).values_list('dimension', flat=True).distinct())
+        dishcat.available_dimension = list_dim
 
-    return render(request, 'orders/menu.html', {"DishCategory": DishCategory.objects.all(),
+
+    return render(request, 'orders/menu.html', {"DishCategory": DishCatProc,
                                                 "Dish": DishProc,
                                                 "Addition": Addition.objects.all()})
+
+
+def get_toppings(request):
+    '''
+    Get the toppings for pizzas!
+    '''
+
